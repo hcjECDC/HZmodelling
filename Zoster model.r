80988149acf3cb6b7da884b18ae235fe3b24584c
Maria <33788128+mariabnd@users.noreply.github.com>
2018-09-18 13:29:09 +0200

    Adding equations from Horn et al

--- Zoster model.r
+++ Zoster model.r
@@ -1,1 +1,80 @@
-# Let's start developing our Herpes Zoster model here....
+# Load required packaged
+library(deSolve)
+library(ggplot2)
+
+# Equations for Horn et al extended SEIR model (https://doi.org/10.1186/s12916-018-1094-7)
+horn <- function(time, state, params){
+  with(as.list(c(state, params)), {
+    # Maternal protection
+    dM <- - b * M + v * M
+    # Susceptible
+    dS <- b * M - (lambda + v) * S
+    # Exposed (latent)
+    dE <- lambda * S - c * E
+    # Infectious
+    dI <- c * E - d * I
+    # Resistant regarding natural varicella
+    dR <- d * I + e * lambda * SZN - f * R
+    # Susceptible vaccinated regarding herpes zoster (natural varicella)
+    dSZN <- - e * lambda * SZN - g * SZN + h * RZN + f * R
+    # Infectious vaccinated regarding herpes zoster (natural varicella)
+    dIZN <- g * SZN - i * IZN
+    # Resistant vaccinated regarding herpes zoster (natural varicella)
+    dRZN <- i * IZN - h * RZN
+    # Susceptible regarding breakthrough varicella
+    dSB <- v1e * v * (M + S) - (f + lambda) * SB + vw1 * V1 + vw2 * V2
+    # Susceptible to herpes zoster due to vaccine virus 
+    dSBV <- - lambda * SBV + f * SB - k * g * SBV + vw1 * V1V + vw2 * V2V
+    # Vaccinated against varicella with 1 dose
+    dV1 <- (1 - v1e) * v * (M + S) - (f + vw1 + lambda) * V1
+    # Vaccinated against varicella with 2 doses
+    dV2 <- (1 - v2e) * v2 * (SB + V1) - (f + vw1 + lambda) * V2
+    # Varicella vaccine-related protection boosted to lifelong duration
+    dVL <- lambda * (V1 + V2 + V1V + V2V + SZV) - f * VL
+    # Exposed regarding breakthrough varicella;
+    dEB <- lambda * (SB + SBV) - c * EB
+    # Infectious regarding breakthrough varicella;
+    dIB <- c * EB - d2 * IB
+    # Resistant regarding breakthrough varicella;
+    dRB <- d2 * IB + e * lambda * SZB - f * RB
+    # Susceptible regarding herpes zoster (breakthrough varicella)
+    dSZB <- - e * lambda * SZB - j * g * SZB + h * RZB + f * RB
+    # Infectious regarding herpes zoster (breakthrough varicella)
+    dIZB <- j * g * SZB - i * IZB
+    # Resistant regarding herpes zoster (breakthrough varicella)
+    dRZB <- i * IZB - h * RZB
+    # Vaccinated against varicella (with one dose) and susceptible to HZ due to vaccine virus
+    dV1V <- f * V1 - (k * g + lambda + vw1) * V1V
+    # Vaccinated against varicella (with two doses) and susceptible to HZ due to vaccine virus
+    dV2V <- f * V2 - (k * g + lambda + vw2) * V2V
+    # Susceptible regarding herpes zoster (vaccine virus)
+    dSZV <- e * lambda * SZV - k * g * SZV + h * RZV + f * VL
+    # Infectious regarding herpes zoster (vaccine virus)
+    dIZV <- k * g * (SBV + V1V + V2V + SZV) - i * IZV
+    # Resistant regarding herpes zoster (vaccine virus)
+    dRZV <- i * IZV - h * RZV
+    # Combined in a list
+    out <- list(c(dM, dS, dE, dI, dR, dSZN, dIZN, dRZN, dSB, dSBV, dV1, dV2, dVL,
+                 dEB, dIB, dRB, dSZB, dIZB, dRZB, dV1V, dV2V, dSZV, dIZV, dRZV))
+    return(out)})}
+
+# Solve the above given initial conditions
+model <- function(initial_states, time, parameters){
+  out <- lsoda(y = initial_states, times = time, func = horn, parms = parameters)
+  out <- as.data.frame(out)
+  return(out)}
+
+# Example without force of infection function (values chosen arbitrarily)
+t <- 50
+time <- seq(0, t, by = t / (2 * length(1 : t)))
+initial_states <- c(M = 164, S = 38, E = 115, I = 206, R = 42, SZN = 65,
+                    IZN = 3, RZN = 29, SB = 36, SBV = 44, V1 = 170,
+                    V2 = 109, VL = 129, EB = 163, IB = 29, RB = 108,
+                    SZB = 129, IZB = 65, RZB = 63, V1V = 31, V2V = 11, 
+                    SZV = 156, IZV = 8, RZV = 5)
+parameters <- c(b = 0.74, v = 0.14, lambda = 0.01, c = 0.61, d = 1.18, e = 1.09,
+                f = 1.06, g = 0.02, h = 1.42, i = 1.26, v1e = 0.69, vw1 = 0.47,
+                vw2 = 0.45, k = 3.32, v2e = 1.13, v2 = 0.11, d2 = 2.59, j = 0.43)
+
+ggplot(data = model(initial_states, time, parameters),
+       mapping = aes(x = time, y = S)) + geom_line()
